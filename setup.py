93de11fd7dc4468debbd1962e99295aebf48b1d0
hardikvasa <hnvasa@gmail.com>
2015-08-06 14:57:30 -0400

    setup.py now updated for pip

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-#
+from setuptools import setup
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -18,21 +19,46 @@
 Super S3 command line tool, setup.py
 """
 
-from setuptools import setup, find_packages
+import sys
+from distutils.core import setup
+from codecs import open
+from os import path
 
-
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+	
+	
 setup(
       name='webb',
-      version=0.9,
+      version='0.9.1.2',
       description='An all-in-one Web Crawler, Web Parser and Web Scrapping library!',
       author='Hardik Vasa',
       maintainer='Hardik Vasa',
       author_email='hnvasa@gmail.com',
-      description='An all-in-one Web Crawler, Web Parser and Web Scrapping library!',
-      long_description = read('README.md'),
-      license= "Apache License, Version 2.0",
+      long_description=long_description,
+      license="Apache License, Version 2.0",
       url='https://github.com/hardikvasa/webb',
-      status='Development',
-      py_modules=['webb'],
-      scripts=['webb/webb.py'],
-      )
+      keywords='scraper crawler spider webb',
+	  classifiers=[
+	    'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+	  ],
+	  zip_safe=False,
+      packages=['webb'],
+	  )
